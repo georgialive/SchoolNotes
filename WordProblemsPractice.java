ec7e53e2e34fdc593c78b5fedd767c261c58a124
Georgia <georgia.m.vrana@gmail.com>
2023-01-23 18:31:32 -0500

    first ecplise commit

---
+++ WordProblemsPractice.java
@@ -0,0 +1,24 @@
+package Unit1PrintingTextandImages;
+
+public class WordProblemsPractice {
+
+	public static void main(String[] args) {
+		
+		boolean right = false;
+    	
+    		System.out.println("Miss Karen has a birthday party on Saturday,"
+    			+ "\nand she needs to get enough cupcakes for everyone at the party."
+    			+ "\nThere are 690 people at the party, and each of the cupcakes are $1.23."
+    			+ "\nHow much money will Miss Karen need for half of the people to get a cupcake.");
+    	
+    		System.out.println("\n");
+    	
+    		System.out.println("\"You can do this!\"");
+    	
+    		System.out.println("\n");
+    	
+    		System.out.println("/Note: You will not use addition or subtration.\\");
+
+	}
+
+}
